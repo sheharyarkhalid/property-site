5be2030ee6fccffe8400a0dd63b128cb3dcbb003
Sheharyar Khalid <sheharyarkhalid0313@gmail.com>
2023-04-18 20:37:56 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+    $(document).ready(function() {  
+
+        $('.mobile_res_toggle_btn').click(function () {
+            $('.mobile_res_toggle_content').toggleClass('responsive_bar');
+        });
+        
+
+        }); 
+      
